480430033691166b040354dcb489442e6acb30d9
zhangyitao <zhangyitao@onemt.com.cn>
2020-09-17 02:11:47 +0000

    莫里斯遍历。

--- 二叉树的前序遍历/solve2.py
+++ 二叉树的前序遍历/solve2.py
@@ -8,16 +8,19 @@
 # https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
 
 class Solution:
+    # 其实遍历框架基本是一样的，只是什么时候出这个 序 ，才符合我们要的那个遍历顺序
     def preorderTraversal(self, root: TreeNode) -> List[int]:
         order = []
         while root:
             if not root.left:
+                order.append(root.val)
                 root = root.right
             else:
                 tmp = root.left
                 while tmp.right and tmp.right != root:
                     tmp = tmp.right
                 if not tmp.right:
+                    order.append(root.val)
                     tmp.right = root
                     root = root.left
                 else:
