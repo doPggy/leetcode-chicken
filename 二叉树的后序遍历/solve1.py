cff75b8de0bfa5d721cd7ab49208acdd2ffe2090
zhangyitao <zhangyitao@onemt.com.cn>
2020-09-23 03:45:22 +0000

    后序遍历 solve1 修改。

--- 二叉树的后序遍历/solve1.py
+++ 二叉树的后序遍历/solve1.py
@@ -7,28 +7,18 @@
 # https://leetcode-cn.com/problems/binary-tree-postorder-traversal/
 class Solution:
     def postorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
         stack = [ root, ]
         order = []
-        pre   = None
-        while len(stack) > 0 or root:
-            while root:
-                if root.left:
-                    stack.append(root.left)
-                if root.right:
-                    stack.append(root.right)
-                # 遍历
-                if root.right and not root.left:
-                    root = root.right
-                else:
-                    root = root.left
-            # 一定是叶子节点
+        while len(stack) > 0:
             root = stack.pop()
-            # print(pre.val)
-            # print(root.val)
             order.append(root.val)
-            if root.right and root.right == pre:
-                root = stack.pop()
-            pre  = root
-            root = root.right
-        return order
+            if root.left:
+                stack.append(root.left)
+            if root.right:
+                stack.append(root.right)
+
+        # 倒序输出
+        return order[::-1]
 
