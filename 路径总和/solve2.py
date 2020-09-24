f4bc4347a153508c1d9721bd68e3cfe830c8a349
zhangyitao <zhangyitao@onemt.com.cn>
2020-09-24 09:24:18 +0000

    路径总和 solve2

---
+++ 路径总和/solve2.py
@@ -0,0 +1,10 @@
+# https://leetcode-cn.com/problems/path-sum/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
