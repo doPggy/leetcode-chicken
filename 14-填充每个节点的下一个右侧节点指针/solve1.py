9fe1bfd56933e599ebc28810ea55d6a3d5666a42
zhangyitao <zhangyitao@onemt.com.cn>
2020-09-29 06:33:21 +0000

    填充每个节点的下一个右侧节点指针

---
+++ 14-填充每个节点的下一个右侧节点指针/solve1.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+# https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/
+class Solution:
+    # 首先想到层序遍历
+    # 说实话，这道题要求空间复杂度得是常数级别
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+        r         = root
+        queue     = [ r, ]
+        queue_len = 1
+        while queue_len > 0:
+            while queue_len > 0:
+                r = queue.pop(0)
+                if queue_len > 1:
+                    r.next = queue[0]
+                if r.left:
+                    queue.append(r.left)
+                if r.right:
+                    queue.append(r.right)
+                queue_len -= 1
+            queue_len = len(queue)
+        return root
---
+++ 14-填充每个节点的下一个右侧节点指针/solve2.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return
+        r    = root
+        left = root
+        head = root
+        # 不用遍历到最后一层，只需要到 N - 1 层就可以了
+        while left.left:
+            while head:
+                head.left.next = head.right
+                if head.next:
+                    head.right.next = head.next.left
+                head = head.next
+            left = left.left
+            head = left
+        return root
