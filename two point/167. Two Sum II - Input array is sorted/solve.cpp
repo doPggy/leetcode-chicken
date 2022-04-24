8e112dfac5ae2b3529a0ef631c66bc32843f4803
doPggy <892008955@qq.com>
2022-04-25 00:09:17 +0800

    167

---
+++ two point/167. Two Sum II - Input array is sorted/solve.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int len   = numbers.size();
+        int end   = len - 1;
+        int start = 0;
+        int sum   = 0;
+        // vector<int> ans;
+        // [3 6 3 5 3]
+        // 排好序的可以头尾双指针
+        while(start < end)
+        {
+            sum = numbers[start] + numbers[end];
+            if(sum > target) end--;
+            else if(sum < target) start++;
+            else break;
+            // while(numbers[start] + numbers[end] > target) --end;
+            // while(numbers[start] + numbers[end] < target) ++start;
+        }
+        return vector<int>{start + 1, end + 1};
+    }
+};
