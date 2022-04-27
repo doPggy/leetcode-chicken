8d70a76531bd7e85965ca5b5d06b08903e70350a
doPggy <892008955@qq.com>
2022-04-27 12:00:41 +0800

    88, 142 2 刷

--- two point/142. Linked List Cycle II/solve_2.cpp
+++ two point/142. Linked List Cycle II/solve_2.cpp
@@ -9,6 +9,25 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        
+        ListNode * fast = head;
+        ListNode * slow = head;
+
+        do
+        {
+            // 到顶
+            if(!fast || !fast->next) return nullptr;
+            fast = fast->next->next;
+            slow = slow->next;
+        }while(fast != slow);
+
+        // 第一次相遇后，fast = head，然后再一步步相遇
+        fast = head;
+        while(fast != slow)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        return fast;
     }
 };
--- two point/88. Merge Sorted Array/solve_2.cpp
+++ two point/88. Merge Sorted Array/solve_2.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int pos = m-- + n-- - 1;
+        // 指向有数据的末尾
+        while(m >= 0 && n >= 0)
+        {
+            nums1[pos--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+        }
 
+        // [3, 4, 0, 0] [1, 2]
+        while(n >= 0)
+        {
+            nums1[pos--] = nums2[n--];
+        }
     }
 };
